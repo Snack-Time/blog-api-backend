f41c02b7d1cd8a229b5e39edecbb128b9bdef8f2
Snack-Time <wmcnally1226@gmail.com>
2024-05-25 19:42:25 -0400

    import mongoose and dotenv

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +23,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// MongoDB
+mongoose.set('strictQuery', false);
+const mongoDB = "database"
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
