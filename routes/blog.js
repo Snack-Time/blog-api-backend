60e1b612c04c2972ffca662302b8d2d6f95c1cf0
Snack-Time <wmcnally1226@gmail.com>
2024-05-31 23:59:34 -0400

    fix line that i put a period instead of a comma in

--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const blog_controller = require("../controllers/blogController");
+const comment_controller = require("../controllers/commentController");
+
+// BLOG SPECIFIC ROUTES
 
 router.get('/posts', blog_controller.get_posts_list);
 
@@ -12,5 +15,17 @@ router.get('/posts/:postid', blog_controller.get_post);
 router.put('/posts/:postid', blog_controller.update_post);
 
 router.delete('/posts/:postid', blog_controller.delete_post);
+
+// BLOG AND COMMENT ROUTES
+
+router.post('/posts/:postid/comments', comment_controller.create_comment_on_post);
+
+router.get('/posts/:postid/comments', comment_controller.get_comments_on_post);
+
+router.get('/posts/:postid/comments/:commentid', comment_controller.get_comment);
+
+router.delete('/posts/:postid/comments/:commentid', comment_controller.delete_comment);
+
+router.put('/posts/:postid/comments/:commentid', comment_controller.update_comment);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const user_controller = require('../controllers/userController');
 const comment_controller = require("../controllers/commentController");
 
+// USER SPECIFIC ROUTES
+
 router.get('/users', user_controller.get_all_users)
 
 router.post('/users', user_controller.create_user);
@@ -14,8 +16,12 @@ router.put('/users/:userid', user_controller.update_user);
 
 router.delete('/users/:userid', user_controller.delete_user);
 
+// USER + BLOG POST ROUTES
+
 router.get('/posts/:userid', user_controller.get_posts_by_user);
 
+// USER + COMMENTS ROUTES
 
+router.get('/users/:userid/comments', comment_controller.get_comments_by_user)
 
 module.exports = router;
