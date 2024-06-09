58b0ed25399902add719d2f8761bb23414d38335
Snack-Time <wmcnally1226@gmail.com>
2024-06-08 21:54:20 -0400

    add jwt sign in

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+const Blog = require('../models/blog');
+const User = require('../models/user');
+const Comment = require('../models/comment');
+
+const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('INDEX PAGE');
+  res.send('Hello! I am an API.');
 });
+
+router.post('/sign-in', async function(req, res, next) {
+  const user = await User.findOne({ username: req.body.username })
+  if (!user) {
+    return res.status(401).json({errors: 'Incorrect Username'})
+  }
+  const match = await bcrypt.compare(req.body.password, user.password)
+  if (!match) {
+    return res.status(401).json({errors: 'Incorrect Password'})
+  }
+
+  const token = jwt.sign({ user }, 'secretkey', {
+    expiresIn: "1d",
+  })
+  res.status(200).json({ user, token })
+})
 
 module.exports = router;
